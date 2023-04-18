01f23a9dad44e40cb95307f3503153ec451d13b9
Noah Vaughn <noahdvaughn@gmail.com>
2023-04-18 18:13:12 -0400

    created stlyes folder with main body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import React from 'react'
+import { MainBody } from './styles/Global.styled'
 
 
 
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-   
+      <MainBody/>
     </div>
   )
 }
---
+++ client/src/styles/Global.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const MainBody = styled.div`
+  background-color: '#000';
+  height: 50px;
+  width: 100%;
+`
