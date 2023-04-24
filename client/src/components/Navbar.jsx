e68e93e179d6440b5943f0298895a71c91e7a98e
Noah Vaughn <noahdvaughn@gmail.com>
2023-04-24 17:45:07 -0400

    added navmenu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {
   FlexContainer,
   PaddingContainer,
-  Container
+  Container,
+  BlueText
 } from '../styles/Global.styled'
 
+import { NavbarContainer, Logo, MenuIcon  } from '../styles/Navbar.styled'
+
 import {GiHamburgerMenu} from 'react-icons/gi'
-import { Theme } from '../utils/Theme'
+import { Theme} from '../utils/Theme'
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+
   return (
-    <NavbarContainer>
-      
+    <NavbarContainer bgColor='transparent'>
+        <PaddingContainer top='1.2rem' bottom='1.2rem'>
+        <Container>
+          <FlexContainer justify='space-between'>
+
+            <Logo>
+              Noah Vaughn, <BlueText>Fullstack Developer</BlueText>
+            </Logo>
+
+            <MenuIcon onClick={()=>{
+              setOpenMenu(!openMenu)
+            }}>
+              <GiHamburgerMenu/>
+            </MenuIcon>
+
+          </FlexContainer>
+        </Container>
+
+        {openMenu && <NavMenu/>}
+        </PaddingContainer>
     </NavbarContainer>
   )
 }
