f8daf5b063180a0095029ee1a3e40e371c161ed9
Noah Vaughn <noahdvaughn@gmail.com>
2023-04-29 17:14:13 -0400

    implemented emailjs

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {
   PaddingContainer, 
   Heading,
@@ -9,8 +9,27 @@ import {
 import { ContactForm, FormInput, FormLabel } from '../styles/Footer.styled'
 import { motion} from 'framer-motion'
 import { fadeInLeftVariant, fadeInRightVariant, fadeInTopVariant, fadeInBottomVariant } from '../utils/Variants'
+import emailjs from '@emailjs/browser';
 
 const Footer = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_vqwicou', 'contact_form', form.current, 'YaivNhk7xkh_OPUlQ')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+
+
+
+
+
   return (
     <PaddingContainer id='Contact' top='5%' bottom='5%' >
 
@@ -20,38 +39,44 @@ const Footer = () => {
       </Heading>
       <PaddingContainer top='3rem' as={motion.div} variants={fadeInBottomVariant} initial='hidden' whileInView='visible'>
       <FlexContainer justify='center' resJustifyItems='center' responsiveFlex>
-        <ContactForm>
+
+
+        <ContactForm ref={form}>
           <PaddingContainer bottom='2rem'>
             <FormLabel>Name:</FormLabel>
-            <FormInput type='text'
+            <FormInput 
+            type='text'
+            name='user_name'
             placeholder='Enter your name'
             as={motion.input}
-             transition={{ type: "spring", stiffness: 500, damping: 50 }}
+            transition={{ type: "spring", stiffness: 500, damping: 50 }}
             whileTap={{ scale: 0.9 }}
             />
 
           </PaddingContainer>
           <PaddingContainer bottom='2rem'>
             <FormLabel>Email:</FormLabel>
-            <FormInput type='text'
-            placeholder='Enter your Email' as={motion.input} transition={{ type: "spring", stiffness: 500, damping: 50 }}
+            <FormInput 
+            type='text'
+            name='user_email'
+            placeholder='Enter your Email' 
+            as={motion.input} 
+            transition={{ type: "spring", stiffness: 500, damping: 50 }}
             whileTap={{ scale: 0.9 }}/>
-
           </PaddingContainer>
-          <PaddingContainer bottom='2rem'
-          
-          >
+          <PaddingContainer bottom='2rem'>
             <FormLabel>Message:</FormLabel>
             <FormInput 
-            as={motion.textarea} transition={{ type: "spring", stiffness: 500, damping: 50 }}
+            as={motion.textarea} 
+            name='message'
+            transition={{ type: "spring", stiffness: 500, damping: 50 }}
             whileTap={{ scale: 0.9 }}
             type='text'
             placeholder='Enter your Message'/>
-
           </PaddingContainer>
 
           <FlexContainer >
-            <Button>Send Message</Button>
+            <Button onClick={sendEmail}>Send Message</Button>
           </FlexContainer>
 
         </ContactForm>
--- client/src/components/MyProjects.jsx
+++ client/src/components/MyProjects.jsx
@@ -16,35 +16,32 @@ const projectDetails = [
   {
     id: 0,
     project_name: "SakugaConnector",
-    project_desc: "Anime discussion and recommendation platform powered by the MyAnimeList API",
+    project_desc: "Anime discussion and recommendation platform powered by the MyAnimeList API. User-authenticated with JWT and Bcrypt. Users can register and login to create reviews and recommendations of their favorite anime. Deployed using Heroku PostgreSQL cloud database and Surge for the React frontend.",
     tech_stack: ['React', 'Node', 'Express', 'Sequelize', 'PostgreSQL'],
     project_img: project1,
     project_url: 'https://sakugaconnector.surge.sh/',
     github_url: 'https://github.com/noahdvaughn/sakuga_connector',
     reverse: false
-
   },
   {
     id: 1,
     project_name: "TurnTables",
-    project_desc: "Music Review and tracking app powered by the Last.FM album API",
+    project_desc: "Music Review and tracking app powered by the Last.FM album API. Users can search for albums and view details such as songs, release date, artist, and tags. Users can create an account and keep a list of listened to albums and give scores to albums. Frontend built with React and backend built with Express, Sequelize and SQL.",
     tech_stack: ['React', 'Node', 'Express', 'Sequelize', 'PostgreSQL'],
     project_img: project2,
     project_url: 'https://github.com/jonclindner/TurnTables',
     github_url: 'https://github.com/jonclindner/TurnTables',
     reverse: true
-
   },
   {
     id: 2,
     project_name: "Pawns101",
-    project_desc: "Chess Database to create and store top players and openings",
+    project_desc: "Chess Database to create and store top players and openings. Users can add their favorite players with info such as elo, age, nationality and favorite opening. Users can also add openings of up to 10 moves with information such as winrate. Created openings have a link to Chess.com where users can view the next best moves. Data is hosted on Atlas' MongoDB cloud server. React frontend and Express backend are deployed in a monorepo to Heroku",
     tech_stack: ['React', 'Node', 'Mongoose', 'MongoDB'],
     project_img: project3,
     project_url: 'https://pawns101.herokuapp.com/',
     github_url: 'https://github.com/noahdvaughn/Pawns101',
     reverse: false
-
   }
 ]
 
